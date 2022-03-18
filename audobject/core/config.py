06586e2e3080d5ab040d02e92b8886fcf3fafb0b
Johannes Wagner <jwagner@audeering.com>
2022-03-18 10:13:34 +0100

    Update audobject/core/config.py
    Co-authored-by: Hagen Wierstorf <hwierstorf@audeering.com>

--- audobject/core/config.py
+++ audobject/core/config.py
@@ -2,7 +2,18 @@ import audobject.core.define as define
 
 
 class config:
-
+    r"""Get/set configuration values for the :mod:`audobject` module.
+    
+    Example:
+        
+        >>> config.PACKAGE_MISMATCH_WARN_LEVEL
+        1
+        >>> config.PACKAGE_MISMATCH_WARN_LEVEL = 0
+        >>> config.PACKAGE_MISMATCH_WARN_LEVEL
+        0
+        
+    """
+    
     PACKAGE_MISMATCH_WARN_LEVEL = define.PackageMismatchWarnLevel.STANDARD
     """Controls when a warning is shown in case of a package mismatch,
     see :class:`audobject.define.PackageMismatchWarnLevel`."""
