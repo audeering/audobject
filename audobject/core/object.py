96831e0087a3d0081a6083913889ca08962d3ed9
Hagen Wierstorf <hwierstorf@audeering.com>
2022-07-14 13:06:19 +0200

    Fix: audobject.Object.__eq__ work for every object (#49)

--- audobject/core/object.py
+++ audobject/core/object.py
@@ -404,7 +404,13 @@ class Object:
         return hash(self.id)
 
     def __eq__(self, other: 'Object') -> bool:
-        return self.id == other.id
+        if isinstance(other, type(self)):
+            return self.id == other.id
+        else:
+            # it might happen that we need to compare
+            # to other non-supported types, compare
+            # https://github.com/audeering/audinterface/issues/68
+            return False
 
     def __repr__(self) -> str:
         return str(self.to_dict(include_version=False))
