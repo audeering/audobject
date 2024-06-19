b7a6c21ffddcfb12a8703ff4fa975080a17e7c71
Hagen Wierstorf <hwierstorf@audeering.com>
2024-06-19 10:28:04 +0200

    Add support for Python 3.12 (#91)
    * Add support for Python 3.12
    
    * Remove audfactory

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,6 @@ copybutton_prompt_is_regexp = True
 # Mapping to external documentation
 intersphinx_mapping = {
     "audeer": ("https://audeering.github.io/audeer/", None),
-    "audfactory": ("https://audeering.github.io/audfactory/", None),
     "python": ("https://docs.python.org/3/", None),
 }
 
